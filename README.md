7984a7a22a831696041dc29b8e74a6ac21de4ba1
Michael Prendergast <mikeprendergast@gmail.com>
2020-04-13 16:06:47 -0400

    Create

---
+++ README.md
@@ -0,0 +1,4 @@
+# Microfrontend Demonstration Capability #1 Application
+
+Run `npm run serve:single-spa` to start the dev server. This will start the server for this subapplication to listen at `http://localhost:4201/`.
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'capability2App-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'microfrontends-capability2';
+}
